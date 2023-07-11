4fc26f9e3cc12edeaf3c03437b1db8fa54848694
Robert Salicki <robsal12@gmail.com>
2023-07-11 14:08:07 +0200

    Apply requested changes.

--- src/main/java/org/example/Calculate.java
+++ src/main/java/org/example/Calculate.java
@@ -4,24 +4,31 @@ public class Calculate {
     public double add(double a, double b) {
         return a + b;
     }
+
     public double substract(double a, double b) {
         return a - b;
     }
+
     public double multiply(double a, double b) {
         return a * b;
     }
+
     public double divide(double a, double b) {
         return a / b;
     }
+
     public double modulo(double a, double b) {
         return a % b;
     }
+
     public double power(double a, double b) {
         return Math.pow(a,b);
     }
+
     public double sqrt(double a) {
         return Math.sqrt(a);
     }
+
     public double pitagoras(double a, double b) {
         double powA = Math.pow(a, 2);
         double powB = Math.pow(b, 2);
--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -22,34 +22,35 @@ public class Calculator {
                 3. Multiply (*)
                 4. Divide (/)
                 5. Modulo (%)
-                6. Power (pow)
+                6. Power (^)
                 7. Square root (sqr)
                 8. Pythagoras""");
         System.out.println("Please write first number -> enter -> operator (sing or number) -> enter -> second number");
-        while(true) {
-            if(result != 0 && (!sc.hasNextInt())) {
-                    continueIfOperandEntered();
-                    continue;
+        while (true) {
+            if (result != 0 && (!sc.hasNextInt())) {
+                continueIfOperandEntered();
+                continue;
             }
-                firstNum = sc.nextDouble();
-                chooseTheOperation();
-                if(ifSqrChosen()) continue;
-                secondNum = sc.nextDouble();
-                result = switch (operationType) {
-                    case "1", "+" -> calculate.add(firstNum, secondNum);
-                    case "2", "-" -> calculate.substract(firstNum, secondNum);
-                    case "3", "*" -> calculate.multiply(firstNum, secondNum);
-                    case "4", "/" -> calculate.divide(firstNum, secondNum);
-                    case "5", "%" -> calculate.modulo(firstNum, secondNum);
-                    case "6", "pow" -> calculate.power(firstNum, secondNum);
-                    case "7", "sqr" -> calculate.sqrt(firstNum);
-                    default -> 0;
-                };
-                System.out.println("=");
-                System.out.println(result);
+            firstNum = sc.nextDouble();
+            chooseTheOperation();
+            if (ifSqrChosen()) break;
+            secondNum = sc.nextDouble();
+            result = switch (operationType) {
+                case "1", "+" -> calculate.add(firstNum, secondNum);
+                case "2", "-" -> calculate.substract(firstNum, secondNum);
+                case "3", "*" -> calculate.multiply(firstNum, secondNum);
+                case "4", "/" -> calculate.divide(firstNum, secondNum);
+                case "5", "%" -> calculate.modulo(firstNum, secondNum);
+                case "6", "^" -> calculate.power(firstNum, secondNum);
+                case "7", "sqr" -> calculate.sqrt(firstNum);
+                default -> 0;
+            };
+            System.out.println("=");
+            System.out.println(result);
 
         }
     }
+
     private void chooseTheOperation() {
         do {
             operationType = sc.nextLine();
@@ -57,41 +58,45 @@ public class Calculator {
         } while (!validateOperationChosen());
 
     }
+
     private boolean validateOperationChosen() {
-        List<String> oprerators = List.of("1", "+", "2", "-", "3", "*", "4", "/", "5", "%", "6", "pow", "7", "sqr");
-        for (String operand: oprerators) {
-            if(operand.equals(operationType)) {
+        List<String> oprerators = List.of("1", "+", "2", "-", "3", "*", "4", "/", "5", "%", "6", "^", "7", "sqr");
+        for (String operand : oprerators) {
+            if (operand.equals(operationType)) {
                 return true;
             }
         }
         return false;
     }
+
     private void validateIfExitChosen() {
-        if(operationType.equalsIgnoreCase("exit")) {
+        if (operationType.equalsIgnoreCase("exit")) {
             System.exit(0);
         }
     }
+
     private void continueIfOperandEntered() {
-            Calculate calculate = new Calculate();
-            chooseTheOperation();
-            if(ifSqrChosen()) return;
-            secondNum = sc.nextDouble();
-            result = switch (operationType) {
-                case "1", "+" -> calculate.add(result, secondNum);
-                case "2", "-" -> calculate.substract(result, secondNum);
-                case "3", "*" -> calculate.multiply(result, secondNum);
-                case "4", "/" -> calculate.divide(result, secondNum);
-                case "5", "%" -> calculate.modulo(result, secondNum);
-                case "6", "pow" -> calculate.power(result, secondNum);
-                case "7", "sqr" -> calculate.sqrt(result);
-                default -> 0;
-            };
-            System.out.println("=");
-            System.out.println(result);
+        Calculate calculate = new Calculate();
+        chooseTheOperation();
+        if (ifSqrChosen()) return;
+        secondNum = sc.nextDouble();
+        result = switch (operationType) {
+            case "1", "+" -> calculate.add(result, secondNum);
+            case "2", "-" -> calculate.substract(result, secondNum);
+            case "3", "*" -> calculate.multiply(result, secondNum);
+            case "4", "/" -> calculate.divide(result, secondNum);
+            case "5", "%" -> calculate.modulo(result, secondNum);
+            case "6", "^" -> calculate.power(result, secondNum);
+            case "7", "sqr" -> calculate.sqrt(result);
+            default -> 0;
+        };
+        System.out.println("=");
+        System.out.println(result);
     }
+
     private boolean ifSqrChosen() {
         Calculate calculate = new Calculate();
-        if(Objects.equals(operationType, "7") || "sqr".equals(operationType)) {
+        if (Objects.equals(operationType, "7") || "sqr".equals(operationType)) {
             result = calculate.sqrt(firstNum);
             System.out.println("=");
             System.out.println(result);
@@ -99,6 +104,7 @@ public class Calculator {
         }
         return false;
     }
+
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
